89b6715768b67e014b6feb3cbec95aaa49b1c340
James Draper <jd.unchecked@gmail.com>
2023-12-11 20:21:02 +0000

    update parameter name.

--- src/classes/Internal/Timer/Timer.php
+++ src/classes/Internal/Timer/Timer.php
@@ -11,11 +11,11 @@ final class Timer
      * @param Closure(): void $benchmark
      * @return string the time taken in nanoseconds.
      */
-    public function __invoke(Closure $benchmark, int $times): string
+    public function __invoke(Closure $benchmark, int $perRun): string
     {
         $before = hrtime();
 
-        for ($i = 0; $i < $times; $i++) {
+        for ($i = 0; $i < $perRun; $i++) {
             $benchmark();
         }
 
