02fbae5d12f0f29257997267ccafbf344b57fc0b
James Draper <jd.unchecked@gmail.com>
2023-12-11 19:27:49 +0000

    Add class representing a moment in time.

---
+++ src/Moment.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Hourglass;
+
+class Moment
+{
+    private const NANOSECONDS_IN_SECOND = 1000000000;
+
+    public static function fromSecondsAndNanoseconds(int $seconds, int $nanoseconds): self
+    {
+        [$seconds, $nanoseconds] = [(string) $seconds, (string) $nanoseconds];
+
+        return new self(bcadd(
+            bcmul($seconds, (string) self::NANOSECONDS_IN_SECOND),
+            $nanoseconds,
+        ));
+    }
+
+    public function __construct(private readonly string $nanoseconds)
+    {
+    }
+
+    public function toNanoseconds(): string
+    {
+        return $this->nanoseconds;
+    }
+}
---
+++ tests/MomentTest.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use Hourglass\Moment;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class MomentTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('providerSecondsAndNanoseconds')]
+    public function itCanBeCreated(string $expected, int $seconds, int $nanoseconds): void
+    {
+        // Assert that a Moment object can be created from a number of seconds
+        //     and nanoseconds representing a moment in time.
+
+        $moment = Moment::fromSecondsAndNanoseconds($seconds, $nanoseconds);
+
+        $this->assertInstanceOf(Moment::class, $moment);
+        $this->assertSame($expected, $moment->toNanoseconds());
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public static function providerSecondsAndNanoseconds(): array
+    {
+        return [
+            ['1000000001', 1, 1],
+            ['1000000002', 1, 2],
+            ['1000000003', 1, 3],
+            ['123000000456', 123, 456],
+        ];
+    }
+}
